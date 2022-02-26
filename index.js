bd710a2c83084841fee378ff0bc45be525f9cc6f
Shegz <segunemmanuel46@gmail.com>
2022-02-26 10:44:09 +0000

    Added notes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Big 0 Notation
+const nemo =['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
 const large = new Array(10000).fill('nemo');
 
 function findNemo2(fish) {
@@ -11,4 +13,9 @@ function findNemo2(fish) {
 
 }
 
-findNemo2(large)
+findNemo2(nemo)
+
+// this function has the Big 0 of O(n) or Linear Time.
+// It depends on the number of inputs
+// n is an integer mainly the no of inputs in the array or operations because it increaeses the computation time.
+  // it measures on how fast the runtime grows
