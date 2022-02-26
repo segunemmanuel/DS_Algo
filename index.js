61eb3a077fc72c6891b1cee2861797fcfc1453ef
Shegz <segunemmanuel46@gmail.com>
2022-02-26 11:29:53 +0000

    Calculate the Big for line 32 Solved

--- index.js
+++ index.js
@@ -18,4 +18,35 @@ findNemo2(nemo)
 // this function has the Big 0 of O(n) or Linear Time.
 // It depends on the number of inputs
 // n is an integer mainly the no of inputs in the array or operations because it increaeses the computation time.
-  // it measures on how fast the runtime grows
+// it measures on how fast the runtime grows
+
+const boxes=[0,1,2,3,4,5,6]
+function logFirstTwoBoxes(boxes){
+console.log(boxes[0]);//O(1)
+console.log(boxes[4]);//O(1)
+  
+}
+// Constant time
+logFirstTwoBoxes(boxes); //has big O of O(2);
+
+// Calculate the Big O 
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; //O(1)
+  let b = 10; //O(1)
+  let c = 50; //O(1)
+  for (let i = 0; i < input; i++) {
+    let x = i + 1; //O(n)
+    let y = i + 2; //O(n)
+    let z = i + 3;//O(n)
+
+  }
+  for (let j = 0; j < input; j++) {
+    let p = j * 2;//O(n)
+    let q = j * 2;//O(n)
+  }
+
+  let whoAmI = "I don't know"; //O(1)
+}
+
+// Big O= 4+5n
